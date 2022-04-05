fbba966586437fd356a026320c959280cbf11a07
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2022-04-05 18:27:58 +0530

    changed routes for login- admin and SPM

--- lib/Admin/quaterly_reports.dart
+++ lib/Admin/quaterly_reports.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuarterlyReports extends StatelessWidget
 {
@@ -16,39 +17,74 @@ class QuarterlyReports extends StatelessWidget
               padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
               child: Container(
                 alignment: Alignment.center,
-                child: RawMaterialButton(onPressed: () async {},
-                    fillColor: Color(0xFF93C6D3),
-                    splashColor: Color(0xFF93C6D3),
-                    highlightColor: Color(0xFF93C6D3),
-                    constraints: BoxConstraints.tight(Size(300, 225)),
-                    padding: const EdgeInsets.symmetric(vertical: 15.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
+                child: Stack(
+                  children:[
+                    Container(
+                      alignment: Alignment.center,
+                      //margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                      child: RawMaterialButton(onPressed: () async {},
+                        fillColor: Color(0xFF93C6D3),
+                        splashColor: Color(0xFF93C6D3),
+                        highlightColor: Color(0xFF93C6D3),
+                        constraints: BoxConstraints.tight(Size(300, 225)),
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                      ),
                     ),
-                    child: Image.asset("lib/Resources/Checklist.png")
+
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                      alignment: Alignment.center,
+                      child: Image.asset("lib/Resources/visitReport.png",
+                        width: 150,
+                        height: 150,),
+                    ),
+                    Container(
+                        margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        alignment: Alignment.center,
+                        child: Text('Collections Report',
+                            style: GoogleFonts.rubik(color: Colors.black, fontSize: 18))
+                    ),
+
+                    SizedBox(
+                      height: 100,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                        //margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                        child: RawMaterialButton(onPressed: () async {},
+                          fillColor: Color(0xFF93C6D3),
+                          splashColor: Color(0xFF93C6D3),
+                          highlightColor: Color(0xFF93C6D3),
+                          constraints: BoxConstraints.tight(Size(300, 225)),
+                          padding: const EdgeInsets.symmetric(vertical: 15.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                    ),
+                ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                      alignment: Alignment.center,
+                      child: Image.asset("lib/Resources/collectionReport.png",
+                        width: 150,
+                        height: 150,),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      alignment: Alignment.center,
+                      child: Text('Collections Report',
+                        style: GoogleFonts.rubik(color: Colors.black, fontSize: 18))
+                    ),
+                ]
                 ),
               )
               ,),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-              child: Container(
-                alignment: Alignment.center,
-                child: RawMaterialButton(onPressed: () async {},
-                    fillColor: Color(0xFF93C6D3),
-                    splashColor: Color(0xFF93C6D3),
-                    highlightColor: Color(0xFF93C6D3),
-                    constraints: BoxConstraints.tight(Size(300, 225)),
-                    padding: const EdgeInsets.symmetric(vertical: 15.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: Image.asset("lib/Resources/money.png")
-                ),
-              )
-              ,)
+
+
+
           ]),
     );
   }
