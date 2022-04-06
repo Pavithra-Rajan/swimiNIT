b7c31b3e3832a338aaf7506c4cc4996e1834ae2c
playerzero <abingigo@gmail.com>
2022-04-06 20:10:27 +0530

    lkjdjlsf

--- lib/SPM/search.dart
+++ lib/SPM/search.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:swiminit/SPM/Person.dart';
 import 'edit_receipt_details.dart';
+import 'package:http/http.dart' as http;
 
 class SearchPage extends StatefulWidget {
 
-  SearchPage({Key? key}) : super(key: key);
+  const SearchPage({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => SearchPageState();
@@ -14,6 +18,21 @@ class SearchPageState extends State<SearchPage>
 {
   final TextEditingController _membIDController = TextEditingController();
   currState state = currState.searching;
+  String membershipID = "-1";
+  Person p = Person("name", "profileImg", "rollno", "enteredAt", "noOfVisits", "dues", "receiptID", "amtPaid", "datePaid", "role", "mailID", "contact1", "contact2")
+
+  Future getSwimmer() async {
+    var response = await http.get(Uri.parse(
+        'https://swiminit.herokuapp.com/getdetails?membershipID=$membershipID&admin=False'));
+    var data = json.decode(response.body);
+    p.name = data["name"];
+    p.rollno = data["membershipID"];
+    p.dues = data["dues"].toString();
+    p.role = data["roles"];
+    p.mailID = data["emailID"];
+
+    return p;
+  }
 
   @override
   Widget build(BuildContext context) {
